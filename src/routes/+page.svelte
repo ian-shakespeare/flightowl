c94c32e935f25cfff16e59a253466b90123e5d0d
Ian Shakespeare <irshakes11@gmail.com>
2023-02-16 19:50:38 -0700

    small style changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang='ts'>
+    import BoundingBox from '$lib/components/UI/BoundingBox.svelte'
     import AirportInput from '$lib/components/AirportInput.svelte'
     import ColorPalette from '$lib/components/ColorPalette.svelte'
     import Login from '$lib/components/Login.svelte'
@@ -18,16 +19,18 @@
 </script>
 
 <ColorPalette />
-{#if !isSignedIn}
-    <Login />
-{:else}
-    <div transition:fade='{{ delay: 100 }}'>
-        You are logged in
-        <button on:click={() => getUsers()}>
-            Get Users
-        </button>
-        <button on:click={() => auth.set(false)} class='fo-btn'>
-            Logout
-        </button>
-    </div>
-{/if}
+<BoundingBox>
+    {#if !isSignedIn}
+        <Login />
+    {:else}
+        <div transition:fade='{{ delay: 100 }}'>
+            You are logged in
+            <button on:click={() => getUsers()}>
+                Get Users
+            </button>
+            <button on:click={() => auth.set(false)} class='fo-btn'>
+                Logout
+            </button>
+        </div>
+    {/if}
+</BoundingBox>
